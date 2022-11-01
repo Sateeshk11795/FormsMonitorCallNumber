e53981cadc17cefaeeed16845fa9bca57a109ffe
Sateesh Maurya <sateeshk11795@gmail.com>
2022-11-01 12:55:55 +0530

    MessagingCenter

--- XFormsMonitorCallNumber.Android/PhoneStateChangedRceiver.cs
+++ XFormsMonitorCallNumber.Android/PhoneStateChangedRceiver.cs
@@ -34,7 +34,7 @@ namespace XFormsMonitorCallNumber.Droid
 
 
                         var incomingPhoneNumber = intent.Extras.GetString(TelephonyManager.ExtraIncomingNumber);
-                        DependencyService.Get<IMessage>().LongTime("from IMessage: You get the InCommingCallNumber is: " + incomingPhoneNumber);
+                        //DependencyService.Get<IMessage>().LongTime("from IMessage: You get the InCommingCallNumber is: " + incomingPhoneNumber);
                         MoveApllicationToFront();
                         //var incomingPhoneNumber = intent.GetStringExtra(TelephonyManager.ExtraIncomingNumber);
                         if (incomingPhoneNumber != null)
